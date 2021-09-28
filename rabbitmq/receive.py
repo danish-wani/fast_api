b5d5e12e20081e32975d3b961460ffadfa767325
Danish Wani <danishwani126@gmail.com>
2021-09-28 17:40:01 +0530

    -Publish multiple messages

--- rabbitmq/receive.py
+++ rabbitmq/receive.py
@@ -1,18 +1,23 @@
 import pika
 import sys
-import os
+import time
 
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='job_queue')
 
     def callback(ch, method, properties, body):
+        print(ch, '...ch...', type(ch), '????', ch.__dict__)
+        print(method, '...method...', type(method), '????', method.__dict__)
+        print(properties, '...properties...', type(properties), '????', properties.__dict__)
         print(" [x] Received %r" % body)
+        print('[x] Sleeping for 10 secs')
+        time.sleep(10)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='job_queue', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- rabbitmq/send.py
+++ rabbitmq/send.py
@@ -1,10 +1,24 @@
 import pika
+import sys
 
+
+msg = str(sys.argv[2])
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='job_queue')
 
-channel.basic_publish(exchange='', routing_key='hello', body=b'Hello World!')
-print(" [x] Sent 'Hello World!'")
+channel.basic_publish(exchange='', routing_key='job_queue', body=bytes(msg, encoding='utf8'),
+                      properties=pika.spec.BasicProperties(content_type='text', content_encoding='utf-8',
+                                                           priority=1))
+
+
+channel.basic_publish(exchange='', routing_key='job_queue', body=bytes(msg, encoding='utf8'),
+                      properties=pika.spec.BasicProperties(content_type='text', content_encoding='utf-8',
+                                                           priority=2))
+
+channel.basic_publish(exchange='', routing_key='job_queue', body=bytes(msg, encoding='utf8'),
+                      properties=pika.spec.BasicProperties(content_type='text', content_encoding='utf-8',
+                                                           priority=3))
+print(f" [x] Sent '{msg}'")
 connection.close()
